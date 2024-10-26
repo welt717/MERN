c67a4a6caa3884c07d029e8f5cd423ad6cb7390e
welt717 <mumohpeter2030@gmail.com>
2024-10-26 19:17:58 +0000

    variants  in  typography

---
+++ material/src/components/home.jsx
@@ -0,0 +1,18 @@
+
+import React   from  'react'
+import Typography from '@mui/material/Typography';
+
+function   Home()
+
+{
+    return(
+        <div> 
+           <Typography   variant="h1"> 
+                 How  world  this  is s my  first   typography text
+            <Typography/>
+        <div/>
+    )
+}
+
+
+export   default Home
