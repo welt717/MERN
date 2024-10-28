cf08e00b7f6fee26d97dacdb0138074e33ca5db2
welt717 <mumohpeter2030@gmail.com>
2024-10-28 13:34:48 +0000

    material   ui

--- material/src/App.jsx
+++ material/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import    Home   from  './components/home'
-function App() {
-  return (
-    <>
+import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Container, Typography, Button } from '@mui/material';
+import Home from "./components/home"; // Adjust your imports as needed
+import "./App.css";
 
-    <Home/>
-    
-    </>
-  )
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#00ff11',
+        },
+        secondary: {
+            main: 'orange',
+        },
+    },
+});
+
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <Typography variant="h4">Welcome to My App</Typography>
+                <Home /> {/* Assuming you want to render the Home component */}
+                <Button variant="contained" color="primary">
+                    Click Me
+                </Button>
+            </Container>
+        </ThemeProvider>
+    );
 }
 
-export default App
+export default App;
--- material/src/components/home.jsx
+++ material/src/components/home.jsx
@@ -1,18 +1,68 @@
-
-import React   from  'react'
+import React from "react";
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import SendIcon from '@mui/icons-material/Send';
+import { makeStyles } from '@mui/styles'; 
+import SendToMobileTwoToneIcon from '@mui/icons-material/SendToMobileTwoTone';
 
-function   Home()
+const useStyles = makeStyles({
+    btn: {
+        color: 'red',
+        backgroundColor: 'pink',
+        padding: '10px',
+        fontSize: '20px', // Adjust font size if needed
+    }
+});
 
-{
-    return(
-        <div> 
-           <Typography   variant="h1"> 
-                 How  world  this  is s my  first   typography text
-            <Typography/>
-        <div/>
-    )
+function Home() {
+    const classes = useStyles();
+
+    return (
+        <Container className="home">
+            <Typography variant="h4">
+                Hello, this is the home component
+            </Typography>
+
+            <Button
+                color="primary" 
+                sx={{ marginTop: 2 }}
+                variant="outlined"
+                className={classes.btn} // Ensure class is applied
+            >
+                Click Here
+            </Button>
+
+            <Button
+                 sx={{
+                    color: 'red',
+                    backgroundColor: 'pink',
+                    padding: '10px',
+                    fontSize: '70px',
+                }}
+
+                onClick={() => {
+                    console.log({
+                        message: "My name is Peter Mumo"
+                    });
+                }}  
+                type="submit"
+                variant="contained" 
+                startIcon={<SendIcon />}
+                endIcon={<SendIcon />}
+                color="secondary"
+            >
+                Click To See My Name
+            </Button>
+
+            <br />
+
+            <SendToMobileTwoToneIcon color="secondary" fontSize="large" /> <br />
+            <SendToMobileTwoToneIcon color="secondary" fontSize="small" /> <br />
+            <SendToMobileTwoToneIcon color="error" fontSize="medium" /> <br />
+            <SendToMobileTwoToneIcon color="disabled" fontSize="small" /> <br />
+        </Container>
+    );
 }
 
-
-export   default Home
+export default Home;
